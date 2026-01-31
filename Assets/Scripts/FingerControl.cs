10535b4fd17a7d4514fe93321737b18b557fb157
sergimol <sergimol@ucm.es>
2026-01-31 18:54:46 +0100

    Habemus juego

--- Assets/Scripts/FingerControl.cs
+++ Assets/Scripts/FingerControl.cs
@@ -4,28 +4,22 @@ using static UnityEngine.InputSystem.InputAction;
 
 public class FingerControl : MonoBehaviour
 {
-    bool leftAttacking, rightAttacking;
     Vector2 leftAxes = new Vector2(), rightAxes = new Vector2();
 
-    Vector3 lHeadIniPos;
+    Vector3 lHeadIniPos, rHeadIniPos;
 
     [SerializeField]
-    Vector3 leftHeadLimits;
+    Vector3 leftHeadLimits, rightHeadLimits;
+
+    float leftAttack = 0, rightAttack = 0;
 
     [SerializeField]
-    float maxForce;
-
-    float leftAttack = 0;
-
-    [SerializeField]
-    Transform leftHead, leftAttackBone, rightMoveBone, rightAttackBone;
-
-    [SerializeField]
-    Rigidbody leftRB;
+    Transform leftHead, rightHead;
 
     private void Start()
     {
         lHeadIniPos = leftHead.position;
+        rHeadIniPos = rightHead.position;
     }
 
     public void OnLeftFingerMove(CallbackContext context)
@@ -35,7 +29,7 @@ public class FingerControl : MonoBehaviour
 
     public void OnRightFingerMove(CallbackContext context)
     {
-        Debug.Log("Right " + context.ReadValue<Vector2>());
+        rightAxes = context.ReadValue<Vector2>();
     }
 
     public void OnLeftFingerAttack(CallbackContext context)
@@ -45,49 +39,21 @@ public class FingerControl : MonoBehaviour
 
     public void OnRightFingerAttack(CallbackContext context)
     {
-        if (context.ReadValue<float>() >= 0.9 && !rightAttacking)
-        {
-            rightAttacking = true;
-        }
-        else if (rightAttacking && context.ReadValue<float>() <= 0.1)
-            rightAttacking = false;
-    }
-
-    public void FixedUpdate()
-    {
-        //if (Mathf.Abs(leftAxes.x) >= 0.1 || Mathf.Abs(leftAxes.y) >= 0.1)
-        //{
-        //    Vector3 xForce = leftRB.GetAccumulatedForce();
-        //    if (Mathf.Abs(xForce.x) < maxForce)
-        //        leftRB.AddForce(new Vector3(leftAxes.x * forceFactor, 0, 0));
-        //    if (Mathf.Abs(xForce.z) < maxForce)
-        //        leftRB.AddForce(new Vector3(0, 0, leftAxes.y * forceFactor));
-
-        //}
-
-        //if (Mathf.Abs(leftHead.position.x - lHeadIniPos.x) >= leftHeadLimits.x)
-        //{
-        //    leftRB.linearVelocity = new Vector3(0, leftRB.linearVelocity.y, leftRB.linearVelocity.z);
-        //    leftHead.position = new Vector3((lHeadIniPos.x + leftHeadLimits.x) * Mathf.Sign(leftHead.position.x), leftHead.position.y, leftHead.position.z);
-        //}
-        //if (Mathf.Abs(leftHead.position.z - lHeadIniPos.z - leftAttack * 2) >= leftHeadLimits.z)
-        //{
-        //    leftRB.linearVelocity = new Vector3(leftRB.linearVelocity.x, leftRB.linearVelocity.y, 0);
-        //    leftHead.position = new Vector3(leftHead.position.x, leftHead.position.y, (lHeadIniPos.z + leftHeadLimits.z) * Mathf.Sign(leftHead.position.z) + leftAttack * 2);
-        //}
+        rightAttack = context.ReadValue<float>();        
     }
 
     public void Update()
     {
         leftHead.position = new Vector3(lHeadIniPos.x + (leftAxes.x * leftHeadLimits.x), lHeadIniPos.y - (leftHeadLimits.y * leftAttack), lHeadIniPos.z + (leftAxes.y * leftHeadLimits.z) + (leftHeadLimits.y * leftAttack));
+        rightHead.position = new Vector3(rHeadIniPos.x - (rightAxes.x * rightHeadLimits.x), rHeadIniPos.y - (rightHeadLimits.y * rightAttack), rHeadIniPos.z - (rightAxes.y * rightHeadLimits.z) - (rightHeadLimits.y * rightAttack));
     }
 
-    private void OnDrawGizmos()
-    {
-        //Gizmos.DrawCube(lHeadIniPos, leftHeadLimits);
-        Gizmos.color = Color.white;
-        Gizmos.DrawWireCube(lHeadIniPos, leftHeadLimits * 2);
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireCube(new Vector3(lHeadIniPos.x, lHeadIniPos.y, lHeadIniPos.z + leftAttack * 2), leftHeadLimits * 2);
-    }
+    //private void OnDrawGizmos()
+    //{
+    //    //Gizmos.DrawCube(lHeadIniPos, leftHeadLimits);
+    //    Gizmos.color = Color.white;
+    //    Gizmos.DrawWireCube(lHeadIniPos, leftHeadLimits * 2);
+    //    Gizmos.color = Color.red;
+    //    Gizmos.DrawWireCube(new Vector3(lHeadIniPos.x, lHeadIniPos.y, lHeadIniPos.z + leftAttack * 2), leftHeadLimits * 2);
+    //}
 }
